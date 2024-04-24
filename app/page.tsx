64b14c5c43473faebf308105839439efe70edfbf
0Chronic <kyawn1063@gmail.com>
2024-04-24 13:36:23 +0630

    added

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,16 @@
 
 import { use, useState } from "react";
 import axios from "axios";
-import { TbBasketSearch } from "react-icons/tb";
+import { BsSearch } from "react-icons/bs";
 
 const url = `https://api.openweathermap.org/data/2.5/weather?q=dubai&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
 
 
 export default function Home() {
-
   const [city ,setCity] = useState('')
   const [weather , setWeather] = useState({})
   const [loading , setLoading] = useState (false)
+ 
 
   const fetchWeather = (e) => {
     e.preventDefault()
@@ -24,11 +24,33 @@ export default function Home() {
     setCity('')
     setLoading(false)
   }
-  return (
-    <div>
-    <img src="https://images.unsplash.com/photo-1601134467661-3d775b999c8b?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="object-cover"  />
-    </div>
-  );
+
+  
+
+return (
+<div>
+   {/*overlay*/}
+  <div className="absolute top-0 left-0 bottom-0 bg-black/40 z-[1]" />
+  <img src='https://images.unsplash.com/photo-1601134467661-3d775b999c8b?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' layout='fill' className='object-cover' />
+  
+  <div className="relative flex justify-between items-center max-w-[500px] w-full m-ato">
+    <form>
+     <div>
+     <input type="text" placeholder="Searchcity" />
+     </div>
+     <button onClick={fetchWeather}><BsSearch></BsSearch></button>
+    </form>
+  </div>
+
+  </div>
+
+
+
+)
    
+}
+
+ 
+
    
  
